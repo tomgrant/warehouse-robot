cf1848f5f97a0cd296bea51511d3fd3087c06a9e
Tom <tomgrant1993@gmail.com>
2023-04-18 22:39:34 +1000

    init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # warehouse-robot
-Programming exercise handling a robot movement around a factory
+Programming exercise handling a robot movement around a factory.
+
+Input accepts commands seperated by a space. For example, input "N N E E S W"
---
+++ robot.js
@@ -0,0 +1,74 @@
+const Y_UPPER_LIMIT = 10;
+const X_UPPER_LIMIT = 10;
+const Y_LOWER_LIMIT = 1;
+const X_LOWER_LIMIT = 1;
+
+const movementCommands = ['N', 'S', 'E', 'W'];
+
+/**
+ * Handle position updating from the command input accounting for grid bounds
+ * 
+ * @function
+ * @template position
+ * @param {position} position 
+ * @param {string} command 
+ * @returns {position} 
+ */
+function moveRobot(position, command) {
+    switch(command) {
+        case 'N':
+            if (position.Y < Y_UPPER_LIMIT) {
+                position.Y++;
+            }
+            break;
+        case 'S':
+            if (position.Y > Y_LOWER_LIMIT) {
+                position.Y--;
+            }
+            break;
+        case 'E':
+            if (position.X < X_UPPER_LIMIT) {
+                position.X++;
+            }
+            break;
+        case 'W':
+            if (position.X > X_LOWER_LIMIT) {
+                position.X--;
+            }
+            break;
+        default:
+            break;
+    }
+    return position;
+}
+
+/**
+ * Take the input value and set initial position 
+ * 
+ * @param {string} commandInput 
+ */
+function handleCommand(commandInput) {
+    // Set initial position of robot
+    let position = {
+        X: 1,
+        Y: 1
+    }
+
+    // Split the input value into an array of characters
+    const commands = commandInput.split(" ");
+    // Iterate through each comman and call moveRobot function
+    commands.forEach(command => {
+        if (movementCommands.includes(command)) {
+            position = moveRobot(position, command);
+        }
+    });
+
+    // Update the positionX and positionY elements with the new values
+    document.getElementById('positionX').innerHTML = position.X;
+    document.getElementById('positionY').innerHTML = position.Y;
+}
+
+document.getElementById('robotCommandInput').addEventListener('input', function () {
+    const commandInput = this.value.toUpperCase();
+    handleCommand(commandInput);
+});
